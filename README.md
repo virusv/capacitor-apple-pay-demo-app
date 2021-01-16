373a3b4b596951b5b6544a83b56775389effe841
Ivan Nalivayko <nalivayko.rus@gmail.com>
2021-01-16 16:41:53 +0300

    update version plugin

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-## Created with Capacitor Create
+# Demo application for plugin capacitor-apple-pay
 
-This app was created using the `npx @capacitor/cli create` command, and comes with a very
-minimal shell for building an app.
+Plugin: [capacitor-apple-pay](https://github.com/virusv/capacitor-apple-pay)
 
-Note: unless you know what you're doing, we don't recommend using the `create` way of building an app. Instead, you'll
-likely want to create an app using your framework's tooling (such as `create-react-app`), and then *add* capacitor
-to *that* project (using `npx @capacitor/cli init`).
-
-### Running this example
-
-To run the provided example, can use serve command:
-
-```bash
-npx cap serve
-```
--- www/js/script.js
+++ www/js/script.js
@@ -1,8 +1,28 @@
 const { ApplePay } = Capacitor.Plugins;
 
 const applePayBtn = document.getElementById('applePay');
-applePayBtn.addEventListener('click', event => {
+const informationBlock = document.getElementById('informationBlock');
+
+applePayBtn.addEventListener('click', async event => {
   event.preventDefault();
+
+  const networks = ['mastercard', 'visa'];
+  const capabilities = ['capability3DS', 'capabilityCredit', 'capabilityDebit'];
+
+  const { isPayment } = await ApplePay.canMakePayments({
+    usingNetworks: networks,
+    capabilities: capabilities,
+  });
+
+  // OR await ApplePay.canMakePayments(<void>);
+
+  if (isPayment) {
+    informationBlock.innerHTML = 'Payments supported!!!';
+  } else {
+    applePayBtn.setAttribute('disabled', 'disabled');
+    informationBlock.innerHTML = 'Payment not supported!';
+    return;
+  }
 
   const paymentRequest = {
     merchantIdentifier: "com.apple.testing",
@@ -11,11 +31,37 @@ applePayBtn.addEventListener('click', event => {
         label: 'order #1001:',
         amount: 120.57,
       }
-    ]
+    ],
+
+    currencyCode: "USD",
+    countryCode: "US",
+
+    supportedNetworks: networks,
+    merchantCapabilities: capabilities,
+
+    requiredShippingContactFields: ['emailAddress', 'phoneNumber', 'name'],
+    requiredBillingContactFields: ['emailAddress', 'phoneNumber', 'name'],
   };
 
-  ApplePay.makePaymentRequest(paymentRequest).then(data => {
-    console.log('data: ', data);
+  try {
+    const paymentResponse = await ApplePay.makePaymentRequest(paymentRequest);
+    
+    const paymentData = paymentResponse.token.paymentData;
+    // INFO: Check and completion of the payment by your processing center
+    console.log(paymentData);
+
+    // DEMO PRINT:
+    informationBlock.innerHTML = JSON.stringify(paymentResponse, null, '  ');
+
+    // If success
     ApplePay.completeLastTransaction({ status: "success" });
-  });
+    // Or error
+    // ApplePay.completeLastTransaction({ status: "error" });
+  } catch (e) {
+    if (e.message === 'canceled') {
+      informationBlock.innerHTML = 'Payment widget was closed by user';
+    } else {
+      informationBlock.innerHTML = 'Unknown error';
+    }
+  }
 });
